cce79ae05b3f741334134e6b2b8fecea5afc2e26
ansep <andreasepielli97@gmail.com>
2023-05-12 12:53:54 +0200

    geofence, track positions and mock gps updates

--- user-interface/src/app/map/map.component.ts
+++ user-interface/src/app/map/map.component.ts
@@ -11,6 +11,7 @@ export class MapComponent {
   private map: L.Map | undefined;
   private gps: L.Marker | undefined;
   private pet: L.Marker | undefined;
+  private geoFence: L.Polygon<any> | undefined;
   private gpsIcon = L.icon({
     iconUrl: 'assets/gps.png',
     iconSize: [40, 40],
@@ -26,6 +27,7 @@ export class MapComponent {
     this.map = L.map('map', {
       center: [41.891106645704795, 12.503608718298109],
       zoom: 18,
+      attributionControl: false,
     });
 
     const tiles = L.tileLayer(
@@ -37,6 +39,13 @@ export class MapComponent {
     );
 
     tiles.addTo(this.map);
+
+    this.geoFence = L.polygon(this.petService.getGeofence(), {
+      color: 'green',
+      fillColor: 'green',
+      fillOpacity: 0.3,
+    });
+    this.geoFence.addTo(this.map);
   }
 
   constructor(private petService: PetService) {}
@@ -44,48 +53,80 @@ export class MapComponent {
   ngAfterViewInit(): void {
     this.initMap();
     if (this.map) {
-      this.gotoGps();
+      this.initGps();
+      this.initPet();
+      this.trackPet();
+      this.trackGps();
+    }
+  }
+
+  initGps(): void {
+    if (this.map) {
+      this.map.locate({ watch: true, enableHighAccuracy: true });
+      this.map.on('locationfound', (e) => {
+        if (!this.gps && this.map) {
+          this.gps = L.marker(e.latlng, {
+            icon: this.gpsIcon,
+          });
+          this.gps.addTo(this.map);
+          this.map.flyTo(e.latlng, 18);
+        }
+      });
+    }
+  }
+
+  initPet(): void {
+    if (this.map) {
+      this.pet = L.marker(this.petService.getPetPosition(), {
+        icon: this.petIcon,
+      });
+      this.pet.bindPopup(
+        'Last position update: ' +
+          new Date(this.petService.getPetPosition().timestamp).toLocaleString(
+            'it'
+          )
+      );
+      this.pet.addTo(this.map);
     }
   }
 
   gotoGps(): void {
-    if (this.map) {
-      if (!this.gps) {
-        this.map.locate({ watch: true, enableHighAccuracy: true });
-        this.map.on('locationfound', (e) => {
-          if (!this.gps && this.map) {
-            this.gps = L.marker(e.latlng, {
-              icon: this.gpsIcon,
-            });
-            this.gps.addTo(this.map);
-            this.map.flyTo(e.latlng, 18);
-          } else if (this.gps) {
-            this.gps.setLatLng(e.latlng);
-          }
-        });
-      } else {
-        this.map.flyTo(this.gps.getLatLng(), 18);
-      }
+    if (this.map && this.gps) {
+      this.map.flyTo(this.gps.getLatLng(), 18);
     }
   }
 
   gotoPet(): void {
-    if (this.map) {
-      if (!this.pet) {
-        this.pet = L.marker(this.petService.getPetPosition(), {
-          icon: this.petIcon,
-        });
+    if (this.map && this.pet) {
+      this.map.flyTo(this.pet.getLatLng(), 18);
+    }
+  }
+
+  // update periodically the pet position
+  trackPet(): void {
+    setInterval(() => {
+      if (this.pet) {
+        this.pet.setLatLng(this.petService.getPetPosition());
         this.pet.bindPopup(
           'Last position update: ' +
             new Date(this.petService.getPetPosition().timestamp).toLocaleString(
               'it'
             )
         );
-        this.pet.addTo(this.map);
-        this.map.flyTo(this.pet.getLatLng(), 18);
-      } else {
-        this.map.flyTo(this.pet.getLatLng(), 18);
       }
-    }
+    }, 10000);
+  }
+  //update periodically the gps position
+  trackGps(): void {
+    setInterval(() => {
+      if (this.map) {
+        this.map.locate({ watch: true, enableHighAccuracy: true });
+        this.map.on('locationfound', (e) => {
+          if (this.gps) {
+            this.gps.setLatLng(e.latlng);
+          }
+        });
+      }
+    }, 10000);
   }
 }
--- user-interface/src/app/pet.service.ts
+++ user-interface/src/app/pet.service.ts
@@ -6,7 +6,21 @@ import { Injectable } from '@angular/core';
 export class PetService {
   constructor() {}
 
-  getPetPosition() {
-    return { lat: 41.8941, lng: 12.495, timestamp: Date.now() - 10000 };
+  getPetPosition(): { lat: number; lng: number; timestamp: number } {
+    return {
+      lat: 41.8941 + (Math.random() - 0.5) / 100,
+      lng: 12.495 + (Math.random() - 0.5) / 100,
+      timestamp: Date.now() - 10000,
+    };
+  }
+
+  getGeofence(): [number, number][] {
+    return [
+      [41.89615593693024, 12.48712220789482],
+      [41.90194818191196, 12.497284633845346],
+      [41.89046959691638, 12.49905267353532],
+      [41.88567734430687, 12.489890247584794],
+      [41.89136368987694, 12.477959781944294],
+    ];
   }
 }
